cf6bdc7bb52f7af80105030ebd70e7986f1957a9
Philip McGrath <philip@philipmcgrath.com>
2019-02-11 21:42:19 -0500

    BUILD print from python

--- py-w64-test-tmp.py
+++ py-w64-test-tmp.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 
 from ctypes import CDLL
 
-CDLL("C:\\projects\\native-libgit2-pkgs\\libgit2-win32-x86_64\\git2.dll")
+print(CDLL("C:\\projects\\native-libgit2-pkgs\\libgit2-win32-x86_64\\git2.dll"))
 

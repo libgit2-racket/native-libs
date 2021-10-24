bc9bd507b4524afa3be826a08564bb750378d9cc
Philip McGrath <philip@philipmcgrath.com>
2021-10-24 14:20:44 -0400

    fixup libgit2-for-racket.nix

--- nix/libgit2-for-racket.nix
+++ nix/libgit2-for-racket.nix
@@ -1,14 +1,22 @@
 { pkgs ? import <nixpkgs> {}
+, rktLibgit2Version
+, rktLibgit2Checksum ? "0vgpb2175a5dhqiy1iwywwppahgqhi340i8bsvafjpvkw284vazd"
+, rktLibgit2CommonCmakeFlags ? []
+, rktLibgit2FetchGitUrl ? "https://github.com/libgit2/libgit2"
+, rktLibgit2Rev ? "v${rktLibgit2Version}"
 }:
 
 pkgs.libgit2.overrideAttrs (oldAttrs: rec {
-  version = "1.3.0";
+  version = rktLibgit2Version;
 
-  src = fetchFromGithub {
-    owner = "libgit2";
-    repo = "libgit2";
-    rev = "v${version}";
-    sha256 = "0vgpb2175a5dhqiy1iwywwppahgqhi340i8bsvafjpvkw284vazd";
+  src = pkgs.fetchgit {
+    url = rktLibgit2FetchGitUrl;
+    rev = rktLibgit2Rev;
+    sha256 = rktLibgit2Checksum;
   };
 
-});
+  cmakeFlags = rktLibgit2CommonCmakeFlags ++ [
+    "-DTHREADSAFE=ON" # placeholder for platform-specific flags
+  ];
+
+})

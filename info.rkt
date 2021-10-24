89e94421dbf186d81256e904c0c648414328c422
Philip McGrath <philip@philipmcgrath.com>
2021-10-24 04:22:43 -0400

    change to Guix-based build
    This is still somewhat manual, but the old package had impurities that
    broke it anyway, so we might as well.
    
    Built from ca7421f3d95c404834824ebfa086b95492745064

--- info.rkt
+++ info.rkt
@@ -1,12 +1,16 @@
 #lang info
+;; SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+(define pkg-name "libgit2-x86_64-linux")
 (define collection "libgit2")
-(define pkg-desc "native libraries for \"libgit2\" on \"x86_64-linux\"")
 (define version "0.0")
+(define pkg-desc "native libraries for \"libgit2\" on \"x86_64-linux\"")
 (define pkg-authors '(philip))
 
 (define install-platform #rx"^x86_64-linux(?:-natipkg)?$")
-(define copy-foreign-libs '("libgit2.so"))
+(define copy-foreign-libs '("libgit2.so.1.3"))
+
+(define license
+  '((GPL-2.0-only WITH GCC-exception-2.0) AND (Apache-2.0 OR MIT)))
 
 (define deps '("base"))
-

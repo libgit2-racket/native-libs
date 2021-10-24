65863e9ff6f64f4b1d9519fd8048889841c4a863
Philip McGrath <philip@philipmcgrath.com>
2021-10-24 07:33:43 -0400

    initial Guix- and MinGW-based build
    This is still somewhat manual.
    
    Built from ca7421f3d95c404834824ebfa086b95492745064

---
+++ info.rkt
@@ -0,0 +1,16 @@
+#lang info
+;; SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+(define pkg-name "libgit2-x86_64-win32")
+(define collection "libgit2")
+(define version "0.0")
+(define pkg-desc "native libraries for \"libgit2\" on \"x86_64-win32\"")
+(define pkg-authors '(philip))
+
+(define install-platform "win32\\x86_64")
+(define copy-foreign-libs '("libgit2-1.3.dll"))
+
+(define license
+  '((GPL-2.0-only WITH GCC-exception-2.0) AND (Apache-2.0 OR MIT)))
+
+(define deps '("base"))

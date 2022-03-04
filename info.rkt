5f0665b86c0a29e51c7617dd7be95989d1fbf9cf
Philip McGrath <philip@philipmcgrath.com>
2022-03-03 22:14:04 -0500

    uncomment aarch64-macosx and *-win32

--- info.rkt
+++ info.rkt
@@ -1,4 +1,5 @@
 #lang info
+;; SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 (define pkg-name "libgit2-native-libs")
 (define collection 'multi)
@@ -14,14 +15,10 @@
                             #:version ,version]
     ["libgit2-x86_64-macosx" #:platform "x86_64-macosx"
                              #:version ,version]
-    ;; coming soon ...
-    #;
     ["libgit2-aarch64-macosx" #:platform "aarch64-macosx"
                               #:version ,version]
-    #;
     ["libgit2-i386-win32" #:platform "win32\\i386"
                           #:version ,version]
-    #;
     ["libgit2-x86_64-win32" #:platform "win32\\x86_64"
                             #:version ,version]
     ))

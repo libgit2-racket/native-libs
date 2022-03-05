850ea7c95274c0cc608ff56069bd1debfa39e95b
Philip McGrath <philip@philipmcgrath.com>
2022-03-04 21:57:10 -0500

    initial `nix flake`-based build
    While I prefer Guix, `nix flake` supports building for all host
    platforms, and using one approach rather than two reduces headaches.
    
    The contents of this commit were generated by commit:
    
        b901e750f3e9bb8581fb69a017b5780ef3326bab
    
    from:
    
        https://github.com/LiberalArtist/native-libgit2-pkgs/tree/build-scripts
    
    The packages for all host platforms were built on an `x86_64-macosx`
    machine, for now.

--- info.rkt
+++ info.rkt
@@ -1,12 +1,19 @@
 #lang info
+;; SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-(define collection "libgit2")
-(define pkg-desc "native libraries for \"libgit2\" on \"x86_64-macosx\"")
+(define pkg-name "libgit2-x86_64-macosx")
+(define collection 'multi)
+
 (define version "0.0")
+(define pkg-desc "native libraries for \"libgit2\" on \"x86_64-macosx\"")
 (define pkg-authors '(philip))
 
 (define install-platform "x86_64-macosx")
-(define copy-foreign-libs '("libgit2.dylib"))
+(define copy-foreign-libs '("libgit2.1.4.dylib"))
+
+(define license
+  '((GPL-2.0-only WITH GCC-exception-2.0)
+    AND
+    (Apache-2.0 OR MIT)))
 
 (define deps '("base"))
-

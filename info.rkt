93e944902a9d7a8ffb2559f4e9057f71fc9240e6
Philip McGrath <philip@philipmcgrath.com>
2022-03-04 21:55:14 -0500

    initial `nix flake`-based build
    While I prefer Guix, `nix flake` supports building for all host
    platforms, and using one approach rather than two reduces headaches.
    
    The contents of this commit were generated by commit:
    
        b901e750f3e9bb8581fb69a017b5780ef3326bab
    
    from:
    
        https://github.com/LiberalArtist/native-libgit2-pkgs/tree/build-scripts
    
    The packages for all host platforms were built on an `x86_64-macosx`
    machine, for now.

--- info.rkt
+++ info.rkt
@@ -5,23 +5,21 @@
 (define collection 'multi)
 
 (define version "0.0")
-(define pkg-authors
-  '(philip))
 (define pkg-desc "native libraries for \"libgit2\" (meta-package)")
+(define pkg-authors '(philip))
+
+(define license
+  '(Apache-2.0 OR MIT))
 
 (define deps
   `("base"
     ["libgit2-x86_64-linux" #:platform #rx"^x86_64-linux(?:-natipkg)?$"
                             #:version ,version]
-    ["libgit2-x86_64-macosx" #:platform "x86_64-macosx"
-                             #:version ,version]
     ["libgit2-aarch64-macosx" #:platform "aarch64-macosx"
                               #:version ,version]
+    ["libgit2-x86_64-macosx" #:platform "x86_64-macosx"
+                             #:version ,version]
     ["libgit2-i386-win32" #:platform "win32\\i386"
                           #:version ,version]
     ["libgit2-x86_64-win32" #:platform "win32\\x86_64"
-                            #:version ,version]
-    ))
-
-(define license
-  '(Apache-2.0 OR MIT))
+                            #:version ,version]))

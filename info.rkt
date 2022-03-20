3e22714e47c1d8b710785a9c1ba26e4630056261
Philip McGrath <philip@philipmcgrath.com>
2022-03-19 23:30:56 -0400

    v0.0.0.4: install channels.scm as channels.txt
    The contents of this commit were generated from the Nix flake:
    
        github:LiberalArtist/native-libgit2-pkgs?ref=build-scripts
    
    at commit:
    
        2700a945c6a8f814eb0e17d3116bc141406db69e

--- info.rkt
+++ info.rkt
@@ -4,7 +4,7 @@
 (define pkg-name "libgit2-i386-win32")
 (define collection "libgit2")
 
-(define version "0.0.0.3")
+(define version "0.0.0.4")
 (define pkg-desc "native libraries for \"libgit2\" on \"i386-win32\"")
 (define pkg-authors '(philip))
 
